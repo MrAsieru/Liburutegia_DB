4ca15ed2c3c2a91c2affa6eb424dac04b5d3f654
Rojo3100 <unaihurtadopelaezlc@gmail.com>
2021-04-19 20:13:23 +0200

    feat: Liburuzaina garatu, opzioak sortu

--- src/Eredua/ErabiltzaileArrunta.java
+++ src/Eredua/ErabiltzaileArrunta.java
@@ -1,5 +1,6 @@
 package Eredua;
 
+import java.sql.SQLException;
 import java.util.Date;
 import java.util.Observable;
 import java.util.Observer;
@@ -27,81 +28,87 @@ public class ErabiltzaileArrunta extends Observable {
 
     public void getKatalogoa() {
         bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_TAULA_EGUNERATU, SQLManager.getManager().getKatalogoa());
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KAT_TAULA_EGUNERATU
     }
 
     public void getKatalogoa(String pIzena, String pDataBehe, String pDataGoi, String pLengoaia, boolean pEskuragarri) {
         bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_TAULA_EGUNERATU, SQLManager.getManager().getKatalogoa(pIzena, pDataBehe, pDataGoi, pLengoaia, pEskuragarri));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KAT_TAULA_EGUNERATU
     }
 
     public void liburuaErreserbatu(long pIsbn) {
-        SQLManager.getManager().liburuaErreserbatu(this.nan, pIsbn);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_TAULA_EGUNERATU, SQLManager.getManager().getKatalogoa());
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KAT_ERRESERBA_ONDO
-        // ERABARR_KAT_ERRESERBA_TXARTO
+        try{
+            SQLManager.getManager().liburuaErreserbatu(this.nan, pIsbn);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_ERRESERBA_ONDO, SQLManager.getManager().getKatalogoa());
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_ERRESERBA_TXARTO, e.getMessage());
+        }
     }
 
     public void getKolekzioak() {
         bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_TAULA_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_KOLEKZIOAK_EGUNERATU
     }
 
     public void getKolekziokoLiburuak(String pKolekzioa) {
         bistaNotifikatu(NotifikazioMotak.ERABARR_KAT_TAULA_EGUNERATU, SQLManager.getManager().getKolekziokoLiburuak(this.nan, pKolekzioa));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_LIBURUAK_EGUNERATU
     }
 
     public void sortuKolekzioa(String pIzena){
-        SQLManager.getManager().sortuKolekzioa(this.nan, pIzena);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_KOLEKZIOA_SORTU_ONDO
-        // ERABARR_KOL_KOLEKZIOA_SORTU_TXARTO
+        try{
+            SQLManager.getManager().sortuKolekzioa(this.nan, pIzena);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOA_SORTU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOA_SORTU_TXARTO, e.getMessage());
+        }
     }
 
     public void removeKolekzioa(String pKolekzioIzena){
-        SQLManager.getManager().removeKolekzioa(this.nan, pKolekzioIzena);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_KOLEKZIOA_EZABATU_ONDO
-        // ERABARR_KOL_KOLEKZIOA_EZABATU_TXARTO
+        try{
+            SQLManager.getManager().removeKolekzioa(this.nan, pKolekzioIzena);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOA_EZABATU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_KOLEKZIOA_EZABATU_TXARTO, e.getMessage());
+        }
     }
 
     public void addLiburuaKolekziora(String pKolekzioIzena, long pISBN){
-        SQLManager.getManager().addLiburuakKolekziora(this.nan, pKolekzioIzena, pISBN);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_LIBURUA_GEHITU_ONDO
-        // ERABARR_KOL_LIBURUA_GEHITU_TXARTO
+        try{
+            SQLManager.getManager().addLiburuakKolekziora(this.nan, pKolekzioIzena, pISBN);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUA_GEHITU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUA_GEHITU_TXARTO, e.getMessage());
+        }
     }
 
     public void removeLiburuaKolekzioan(String pKolekzioIzena, long pISBN){
-        SQLManager.getManager().removeLiburuakKolekziora(this.nan, pKolekzioIzena, pISBN);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KOL_LIBURUA_KENDU_ONDO
-        // ERABARR_KOL_LIBURUA_KENDU_TXARTO
+        try{
+            SQLManager.getManager().removeLiburuakKolekziora(this.nan, pKolekzioIzena, pISBN);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUAK_EGUNERATU, SQLManager.getManager().getKolekzioak(this.nan));
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUA_KENDU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KOL_LIBURUA_KENDU_TXARTO, e.getMessage());
+        }
     }
 
     public void getErabiltzaileInformazioa(){
         bistaNotifikatu(NotifikazioMotak.ERABARR_KON_INFORMAZIOA_EGUNERATU, SQLManager.getManager().getErabiltzaileInformazioa(this.nan));
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KON_INFORMAZIOA_EGUNERATU
     }
 
     public void erabiltzaileInformazioaEguneratu(String pIzena, String pAbizena, String pPasahitza, String pGeneroa, String pJaioData){
-        SQLManager.getManager().erabiltzaileInformazioaEguneratu(this.nan, pIzena, pAbizena, pPasahitza, pGeneroa, pJaioData);
-        bistaNotifikatu(NotifikazioMotak.ERABARR_KON_INFORMAZIOA_EGUNERATU, SQLManager.getManager().getErabiltzaileInformazioa(this.nan));
-        //TODO (null elementua ez bada aldatu)
-        //TODO erabili behar diren notifikazio motak:
-        // ERABARR_KON_ALDAKETA_ONDO
-        // ERABARR_KON_ALDAKETA_TXARTO
+        try{
+            SQLManager.getManager().erabiltzaileInformazioaEguneratu(this.nan, pIzena, pAbizena, pPasahitza, pGeneroa, pJaioData);
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KON_INFORMAZIOA_EGUNERATU, SQLManager.getManager().getErabiltzaileInformazioa(this.nan));
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KON_ALDAKETA_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.ERABARR_KON_ALDAKETA_TXARTO, e.getMessage());
+        }
     }
 
     private void bistaNotifikatu(NotifikazioMotak pMota, Object ... pArg){
--- src/Eredua/Liburuzaina.java
+++ src/Eredua/Liburuzaina.java
@@ -1,5 +1,6 @@
 package Eredua;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Observable;
@@ -30,99 +31,109 @@ public class Liburuzaina extends Observable{
 
     public void getErabiltzaileak() {
         bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak());
-        //TODO erabiltzaileak filtroekin bilatu eta bueltatu
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ERAB_LISTA_EGUNERATU
     }
 
     public void getErabiltzaileak(String pNan, String pIzena, String pAbizena) {
         bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak(pNan, pIzena, pAbizena));
-        //TODO erabiltzaileak filtroekin bilatu eta bueltatu
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ERAB_LISTA_EGUNERATU
     }
 
     public void addErabiltzaileArrunta(Erabiltzailea pErabiltzaile, String pPasahitza){
-        SQLManager.getManager().addErabiltzaileArrunta(pErabiltzaile, pPasahitza);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ERAB_SORTU_ONDO,
-        // LIBURUZAIN_ERAB_SORTU_TXARTO
-        // LIBURUZAIN_ERAB_LISTA_EGUNERATU
+        try{
+            SQLManager.getManager().addErabiltzaileArrunta(pErabiltzaile, pPasahitza);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_SORTU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_SORTU_TXARTO, e.getMessage());
+        }
     }
 
     public void aldatuPasahitza(String pNan, String pPasahitza) {
-        SQLManager.getManager().aldatuPasahitza(pNan, pPasahitza);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_PASAHITZA_TXARTO, "Pasahitzaren logika ez dago sortuta.");
-        //TODO erabiltzailearen pasahitza aldatu eta bueltatu
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ERAB_PASAHITZA_ONDO
-        // LIBURUZAIN_ERAB_PASAHITZA_TXARTO
+        try{
+            SQLManager.getManager().aldatuPasahitza(pNan, pPasahitza);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_PASAHITZA_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_PASAHITZA_TXARTO, e.getMessage());
+        }
     }
 
     public void removeErabiltzailea(String pNAN){
-        SQLManager.getManager().removeErabiltzaile(pNAN);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ERAB_EZA_ONDO
-        // LIBURUZAIN_ERAB_EZA_TXARTO
-        // LIBURUZAIN_ERAB_LISTA_EGUNERATU
+        try{
+            SQLManager.getManager().removeErabiltzaile(pNAN);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_LISTA_EGUNERATU, SQLManager.getManager().getErabiltzaileak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_EZA_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ERAB_EZA_TXARTO, e.getMessage());
+        }
     }
 
     public void getLiburuak() {
         bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_TAULA_EGUNERATU, SQLManager.getManager().getLiburuak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_KAT_TAULA_EGUNERATU
     }
 
     public void getLengoaiak() {
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_GEH_LENGOAIA_LISTA_EGUNERATU, SQLManager.getManager().getLengoaiak());
+        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_GEH_LENGOAIA_LISTA_EGUNERATU,
+                new String[]{"Euskera", "Gaztelera", "Ingelesa"});
         // Lista hemen sortu, ez da behar DB-n egotea
         //TODO erabili behar diren notifikazio motak:
         // LIBURUZAIN_KAT_GEH_LENGOAIA_LISTA_EGUNERATU
     }
 
     public void addLiburu(Liburua pLiburua){
-        SQLManager.getManager().addLiburu(pLiburua);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_TAULA_EGUNERATU, SQLManager.getManager().getLiburuak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_KAT_GEH_ONDO_SORTUTA
-        // LIBURUZAIN_KAT_GEH_TXARTO_SORTUTA
-        // LIBURUZAIN_KAT_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().addLiburu(pLiburua);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_TAULA_EGUNERATU, SQLManager.getManager().getLiburuak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_GEH_ONDO_SORTUTA);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_GEH_TXARTO_SORTUTA, e.getMessage());
+        }
     }
 
     public void removeLiburu(long ISBN){
-        SQLManager.getManager().removeLiburu(ISBN);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_TAULA_EGUNERATU, SQLManager.getManager().getLiburuak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_KAT_EZA_ONDO_EZABATUTA
-        // LIBURUZAIN_KAT_EZA_TXARTO_EZABATUTA
-        // LIBURUZAIN_KAT_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().removeLiburu(ISBN);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_TAULA_EGUNERATU, SQLManager.getManager().getLiburuak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_EZA_ONDO_EZABATUTA);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_KAT_EZA_TXARTO_EZABATUTA, e.getMessage());
+        }
     }
 
     public void getMaileguak() {
         bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_TAULA_EGUNERATU, SQLManager.getManager().getMaileguak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_MAI_TAULA_EGUNERATU
     }
 
     public void addMailegua(String NAN, long pISBN){
-        SQLManager.getManager().addMailegua(NAN, pISBN);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_TAULA_EGUNERATU, SQLManager.getManager().getMaileguak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_MAI_HASI_ONDO
-        // LIBURUZAIN_MAI_HASI_ERRESERBATUTA
-        // LIBURUZAIN_MAI_HASI_TXARTO
-        // LIBURUZAIN_MAI_TAULA_EGUNERATU
+        //
+        Liburua lib = SQLManager.getManager().getLiburua(pISBN);
+        if (!lib.mailegatuta) {
+            if (!lib.erreserbatua) {
+                SQLManager.getManager().addMailegua(NAN, pISBN);
+                bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_TAULA_EGUNERATU, SQLManager.getManager().getMaileguak());
+                bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_HASI_ONDO);
+            }
+            else {
+                bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_HASI_ERRESERBATUTA);
+            }
+        }
+        else {
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_HASI_TXARTO, "liburua mailegatuta");
+        }
     }
 
     public void removeMailegua(long pISBN){
-        SQLManager.getManager().removeMailegua(pISBN);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_TAULA_EGUNERATU, SQLManager.getManager().getMaileguak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_MAI_BUELTATU_ONDO
-        // LIBURUZAIN_MAI_BUELTATU_TXARTO
-        // LIBURUZAIN_MAI_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().removeMailegua(pISBN);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_TAULA_EGUNERATU, SQLManager.getManager().getMaileguak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_BUELTATU_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_MAI_BUELTATU_TXARTO, e.getMessage());
+        }
     }
 
     public void getIdazleak() {
@@ -133,21 +144,25 @@ public class Liburuzaina extends Observable{
     }
 
     public void addIdazlea(Idazlea pIdl) {
-        SQLManager.getManager().addAutorea(pIdl);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_TAULA_EGUNERATU, SQLManager.getManager().getIdazleak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_IDL_GEH_ONDO
-        // LIBURUZAIN_IDL_GEH_TXARTO
-        // LIBURUZAIN_IDL_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().addAutorea(pIdl);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_TAULA_EGUNERATU, SQLManager.getManager().getIdazleak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_GEH_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_GEH_TXARTO, e.getMessage());
+        }
     }
 
     public void removeIdazlea(String pIdazleZenbakia){
-        SQLManager.getManager().removeAutorea(pIdazleZenbakia);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_TAULA_EGUNERATU, SQLManager.getManager().getIdazleak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_IDL_KEN_ONDO
-        // LIBURUZAIN_IDL_KEN_TXARTO
-        // LIBURUZAIN_IDL_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().removeAutorea(pIdazleZenbakia);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_TAULA_EGUNERATU, SQLManager.getManager().getIdazleak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_KEN_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_IDL_KEN_TXARTO, e.getMessage());
+        }
     }
 
     public void getArgitaletxeak() {
@@ -158,21 +173,25 @@ public class Liburuzaina extends Observable{
     }
 
     public void addArgitaletxea(Argitaletxea arg) {
-        SQLManager.getManager().addArgitaletxea(arg);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_TAULA_EGUNERATU, SQLManager.getManager().getArgitaletxeak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ARG_GEH_ONDO
-        // LIBURUZAIN_ARG_GEH_TXARTO
-        // LIBURUZAIN_ARG_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().addArgitaletxea(arg);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_TAULA_EGUNERATU, SQLManager.getManager().getArgitaletxeak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_GEH_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_GEH_TXARTO, e.getMessage());
+        }
     }
 
     public void removeArgitaletzea(String pIFK){
-        SQLManager.getManager().removeArgitaletzea(pIFK);
-        bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_TAULA_EGUNERATU, SQLManager.getManager().getArgitaletxeak());
-        //TODO erabili behar diren notifikazio motak:
-        // LIBURUZAIN_ARG_KEN_ONDO
-        // LIBURUZAIN_ARG_KEN_TXARTO
-        // LIBURUZAIN_ARG_TAULA_EGUNERATU
+        try{
+            SQLManager.getManager().removeArgitaletzea(pIFK);
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_TAULA_EGUNERATU, SQLManager.getManager().getArgitaletxeak());
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_KEN_ONDO);
+        }
+        catch (SQLException e){
+            bistaNotifikatu(NotifikazioMotak.LIBURUZAIN_ARG_KEN_TXARTO, e.getMessage());
+        }
     }
 
     private void bistaNotifikatu(NotifikazioMotak pMota, Object ... pArg){
